0fa5cb2249cfb7dfdcfe6dea7ca9f88653acdd63
LDN <ledenevdn@gmail.com>
2020-04-27 16:52:36 +0700

    Add gem slim

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module Sqa
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+                       controller_specs: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+    end
   end
 end
