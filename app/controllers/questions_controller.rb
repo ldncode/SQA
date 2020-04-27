19175a9f795789e005a127deff6eda20c485f856
LDN <ledenevdn@gmail.com>
2020-04-27 20:42:32 +0700

    Add rspec test action question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,25 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+
 end
