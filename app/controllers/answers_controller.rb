a3a83f7e216c67199729d1f227d515bb1c0acd11
LDN <ledenevdn@gmail.com>
2020-05-13 21:02:39 +0700

    Add rspec action create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,21 @@
 class AnswersController < ApplicationController
+  before_action :find_question, only: [:create]
 
+  def create
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to @question
+    end
+
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :answer do
     body { "MyText" }
+
+    trait :invalid do
+      body { nil }
+    end
   end
 end
