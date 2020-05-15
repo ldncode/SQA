e764e96fd3de31e5dd08d25cdf479006fc67c588
LDN <ledenevdn@gmail.com>
2020-05-15 20:21:21 +0700

    Add spec feature sign in

---
+++ spec/features/question/create_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'User can create questions', %q{
+  In order to get answer from a community
+  As an authenticated user
+  I'd like to be able to ask the question
+} do
+  scenario 'Authenticated user asks a question' do
+
+  end
+  scenario 'Authenticated user asks a question with errors'
+  scenario 'Unauthenticated user tries asks question'
+end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,6 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
-
   it { should validate_presence_of :body }
 end
