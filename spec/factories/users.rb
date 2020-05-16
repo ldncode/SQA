2151f8f9467f282366415e507977286b69a208c2
LDN <ledenevdn@gmail.com>
2020-05-16 17:57:05 +0700

    Refactoring finish spec feature authenticated user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@test.com"
+  end
   factory :user do
-    
+    email
+    password { '123456' }
+    password_confirmation { '123456' }
   end
 end
