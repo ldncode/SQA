ee394c17cb0052ffbddefefb8b73ae16eea9da76
LDN <ledenevdn@gmail.com>
2020-05-16 17:55:29 +0700

    Add controller and feature helpers

---
+++ spec/support/feature_helpers.rb
@@ -0,0 +1,8 @@
+module FeatureHelpers
+  def sign_in(user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+  end
+end
