07b5dd73e9e1b41c59a69883550167863606936a
LDN <ledenevdn@gmail.com>
2020-05-26 18:27:40 +0700

    Add rspec scenario user can sing out

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -24,3 +24,17 @@ feature 'User can sing in', %q{
     expect(page).to have_content 'Invalid Email or password.'
   end
 end
+
+feature 'User can sing out', %q{
+  As an authenticated user
+  I'd like to be able to sign out
+} do
+  given(:user) { create(:user) }
+
+  scenario 'Registered user tries to sign out' do
+    sign_in(user)
+    click_on 'Logout'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
