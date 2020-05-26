14ce8a88873a1a56c7e2d86b04978daa5ffc76b0
LDN <ledenevdn@gmail.com>
2020-05-26 19:42:28 +0700

    Add rspec scenario user can sing up

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -38,3 +38,21 @@ feature 'User can sing out', %q{
     expect(page).to have_content 'Signed out successfully.'
   end
 end
+
+feature 'User can sign up', %q{
+  As an unauthenticated user
+  I'd like to be able to sign up
+} do
+
+  given(:user) { build(:user) }
+
+  scenario 'Unregistered user tries to sign up' do
+    visit new_user_registration_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password_confirmation
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+end
