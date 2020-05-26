f99ea4ed67336e3d1b16f922473823019bcaeeff
LDN <ledenevdn@gmail.com>
2020-05-26 17:56:16 +0700

    Update rspec

--- spec/features/question/show_questions_spec.rb
+++ spec/features/question/show_questions_spec.rb
@@ -1,27 +1,16 @@
 require 'rails_helper'
 
-feature 'User can view the questions', %q{
-    User can see the question,
-    The user can create an answer to a question,
-    The user should see the answers to the question
-} do
+feature 'User can view the question' do
 
   given(:user) { create(:user) }
   given!(:question) { create(:question) }
 
-  scenario 'list questions' do
-    visit questions_path
+  scenario 'view question' do
+    visit question_path(question)
 
-    expect(page).to have_content 'List questions'
+    expect(page).to have_content 'Question'
     expect(page).to have_content question.title
     expect(page).to have_content question.body
   end
 
-  scenario 'see to question and answers' do
-    visit question_path(question)
-
-    
-
-  end
-  scenario 'create an answer to a question'
 end
