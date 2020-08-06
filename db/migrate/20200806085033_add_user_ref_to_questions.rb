755635f33760a28470d338c28462031e3f45e414
LDN <ledenevdn@gmail.com>
2020-08-06 19:15:17 +0700

    Add migrations user ref

---
+++ db/migrate/20200806085033_add_user_ref_to_questions.rb
@@ -0,0 +1,5 @@
+class AddUserRefToQuestions < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :questions, :user, foreign_key: true
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { is_expected.to validate_presence_of :email }
-  it { is_expected.to validate_presence_of :password }
-
   it { is_expected.to have_many(:questions).dependent(:destroy) }
   it { is_expected.to have_many(:answers).dependent(:destroy) }
+
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_presence_of :password }
 end
