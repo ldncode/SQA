665e15c07e61f2626f86da7f3b92890bf4b5706a
LDN <ledenevdn@gmail.com>
2020-08-06 15:08:56 +0700

    Add update view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :questions, dependent: :destroy
-  has_many :answers, dependent: :destroy
+  has_many :answers
 
 end
