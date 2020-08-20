c728491f078295773efa004917299915ecaca49a
LDN <ledenevdn@gmail.com>
2020-08-20 20:11:26 +0700

    Add question get show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -18,6 +18,19 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns new answer for question' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+  end
+
   describe 'GET #new' do
     before { get :new }
 
